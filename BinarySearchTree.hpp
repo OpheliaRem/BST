95ef03a3d8b1c6b948f95805a0355e15b600fbf7
Artem Vasiukov <vasartvas2@gmail.com>
2025-12-23 02:13:16 +0300

    fix: typo in initializer list of Node constructor was fixed

--- BinarySearchTree.hpp
+++ BinarySearchTree.hpp
@@ -13,7 +13,7 @@ namespace ds {
             Node* left;
             Node* right;
 
-            Node(T data) : data{std::move(data)}, left(nullptr), right(nullptr) {}
+            Node(T data) : data{std::move(data)}, left{nullptr}, right{nullptr} {}
         };
 
         Node* root;
